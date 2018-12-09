0263b2360b8c84e0e10687318c6c2193b183248f
Jowensky Neard <jp.12@live.com>
2018-12-09 16:35:35 -0500

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,45 @@
-var user = require("../data/friends")
+var user = require("../data/friends");
 
-function answerInt (answers) {
+function answerInt(answers) {
   var scoreInt = [];
 
   answers.forEach(function(vaule, index, array) {
-    array[index] = scoreInt.push(parseInt(vaule))
-  })
+    array[index] = scoreInt.push(parseInt(vaule));
+  });
   return scoreInt;
-};
+}
 
 module.exports = function(app) {
-
   app.get("/api/friends", function(req, res) {
     res.json(user);
   });
 
   app.post("/api/friends", function(req, res) {
-    
     newUser = {
       name: req.body.name,
       photo: req.body.photo,
       answers: answerInt(req.body.answers)
+    };
+    // turn into function
+
+    var lowestdiff = [];
+    var lowest = 0;
+    var obj = 0;
+
+    for (var obj in user) {
+      for (var diff in newUser.answers) {
+        lowest += Math.abs(user[obj].answers[diff] - newUser.answers[diff]);
+      }
+      lowestdiff.push(lowest);
     }
 
-    user.push(newUser)
-    totalDifference = 0;
-
-    console.log(user)
-
-    res.json(newUser)
+    var lowestnumber = Math.min(...lowestdiff);
+    var i = lowestdiff.indexOf(Math.min(...lowestdiff));
+    // turn into function
+    console.log(i)
+    console.log(lowestdiff);
+    console.log(lowestnumber);
+    user.push(newUser);
+    res.json(user[i]);
   });
 };
-
