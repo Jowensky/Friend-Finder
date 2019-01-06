311790b08ed67f298e35d5553dca33358ee6f880
Jowensky Neard <jp.12@live.com>
2019-01-06 06:48:10 -0500

    update

--- app/public/javascript/script.js
+++ app/public/javascript/script.js
@@ -1,59 +1,57 @@
 $(document).ready(function() {
-   game.startGame();
+  game.startGame();
 
-    $("body").on("click", "#submit", game.nextQuestion);
-
-  $("#finish").on("click", function() {
-    $.post("/api/friends", user, function(data) {
-      // Data will be posted on modal on return
-      
-      $("#match-name").text(data.name);
-      $("#match-img").attr("src", data.photo);
-    });
-  });
+  $("body").on("click", "#submit", game.nextQuestion);
 });
- 
+
+var user = {
+    name: $("#userName").val(),
+    photo: $("#userPic").val(),
+    answers: []
+};
+
 var Questions = [
-`You believe life is a simulatin`,
-`Creative`,
-`Prefer kickacks over parties`,
-`Sympathetic`,
-`Care about peoples opinion of you`,
-`Open to new experiences`,
-`Talkative`,
-`Critical`,
-`Easily Upset`,
-`Passionate`
+  `You believe life is a simulatin`,
+  `Creative`,
+  `Prefer kickacks over parties`,
+  `Sympathetic`,
+  `Care about peoples opinion of you`,
+  `Open to new experiences`,
+  `Talkative`,
+  `Critical`,
+  `Easily Upset`,
+  `Passionate`
 ];
 
 var game = {
-startGame: function() {
+  startGame: function() {
     game.renderQuestion();
-},
-currentQuestion: 0,
-nextQuestion: function() {
+  },
+  currentQuestion: 0,
+  nextQuestion: function() {
     if (game.currentQuestion < Questions.length - 1) {
-    game.handleSubmit();
-    game.currentQuestion += 1;
-    game.renderQuestion();
+      game.handleSubmit();
+      game.currentQuestion += 1;
+      game.renderQuestion();
     } else {
-    $("#finish").toggle();
+      mate();
     }
-},
-renderQuestion: function() {
+  },
+  renderQuestion: function() {
     $("label").html(Questions[game.currentQuestion]);
-},
-handleSubmit: function() {
+  },
+  handleSubmit: function() {
     selectedAnswer = $("#q").val();
     user.answers.push(selectedAnswer);
-}
+  },
+  mate: function() {
+    $.post("/api/friends", user, function(data) {
+        // Data will be posted on modal on return
+  
+        $("#match-name").text(data.name);
+        $("#match-img").attr("src", data.photo);
+      });
+  }
 };
 
-var user = {
-name: $("#userName")
-    .val(),
-photo: $("#userPic")
-    .val(),
-answers: []
-};
 
